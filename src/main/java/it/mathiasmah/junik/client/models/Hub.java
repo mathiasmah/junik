0156ccb358d1e804ccd777138e441912a87e874a
Mathias Mahlknecht <mathiasmahlknecht@hotmail.com>
2019-08-20 09:42:07 +0200

    add missing Hub field getters and setters

--- src/main/java/it/mathiasmah/junik/client/models/Hub.java
+++ src/main/java/it/mathiasmah/junik/client/models/Hub.java
@@ -5,4 +5,37 @@ public class Hub {
     private String url;
     private String user;
     private String pass;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    @Override
+    public String toString() {
+        return "Hub{" +
+                "url='" + url + '\'' +
+                ", user='" + user + '\'' +
+                ", pass='" + pass + '\'' +
+                '}';
+    }
 }
